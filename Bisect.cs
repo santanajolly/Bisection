61041726a194e220a2b02977b27f9b0277e4d2b3
Santana Jolly <santanaharris@gmail.com>
2019-08-27 08:11:16 -0400

    Updated if statements

---
+++ Bisect.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bisection_Algorithm
+{
+    class Bisect
+    {
+        public int usernum = 0;
+        public int UserInput()
+        {
+            Console.Write("Enter a number from 1 to 10: ");
+            try
+            {
+                int usernum = Int32.Parse(Console.ReadLine());
+                if (usernum > 0 && usernum < 11)
+                {
+                    return usernum;
+                }
+                else
+                {
+                    //Console.Clear();
+                    Console.Write("Invalid Input. Enter a number from 1 to 10:");
+                    Console.ReadLine();
+                    UserInput();
+                    return usernum;
+                }
+            }
+            catch (Exception)
+            {
+                Console.Clear();
+                Console.Write("Invalid Input. Enter a number from 1 to 10:");
+                Console.ReadLine();
+                UserInput();
+                
+            }
+
+            return usernum;
+        }
+        public void Bisection(int usernum, int[] list)
+        {
+            int first = list[0];
+            int last = list[list.Length - 1];
+            int middle = 0;
+            int count = 0;
+
+            while (list[middle] != usernum)
+            {
+                middle = (first + last) / 2;
+                if (list[middle] > usernum)
+                {
+
+                    count++;
+                    last = middle - 1;
+
+                    Console.WriteLine($"The range of numbers is {first} - {last}");
+                    Console.WriteLine($"Your number is less than {middle}.");
+                }
+                else if (list[middle] < usernum)
+                {
+                    count++;
+                    first = middle + 1;
+                    Console.WriteLine($"The range of numbers is {first} - {last}");
+                    Console.WriteLine($"Your number is more than {middle}.");
+                }
+
+            }
+
+            Console.WriteLine($"It took {count} iterations to find {usernum}.");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,62 +8,19 @@ using System.Threading.Tasks;
 
 namespace Bisection_Algorithm
 {
-    class Program
+    public class Program
     {
-        int[] list = new int[10]
-            {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        static void Main(string[] args)
+        
+        public static void Main(string[] args)
         {
-           
-           
-
-
-
+            int[] list = new int[10]
+                {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+            Bisect bi = new Bisect();
+            int usernum = bi.UserInput();
+            bi.Bisection(usernum, list);
         }
 
-        public int UserInput()
-        {
-            try
-            {
-                Console.Write("Enter a number from 1 to 10: ");
-                int usernum = Int32.Parse(Console.ReadLine());
-                return usernum;
-            }
-            catch (Exception)
-            {
-                Console.Clear();
-                Console.Write("Invalid Input. Enter a number from 1 to 10:");
-                int usernum = Int32.Parse(Console.ReadLine());
-                return usernum;
-            }
-            finally
-            {
-                Console.Clear();
-                Console.Write("Invalid Input. Enter a number from 1 to 10:");
-                int usernum = Int32.Parse(Console.ReadLine());
-                
-            }
-            
-        }
-
-        public double Bisection(int usernum)
-        {
-            int first = list[list.Length];
-            int last = list[list.Length - 1];
-            int middle = first + last / 2;
-            int count = 0;
-
-            while (list[middle] != usernum)
-            {
-                count++;
-
-
-            }
-
-            Console.WriteLine($"{usernum} was the middle value in the array. It took {count} iterations to find your number!");
-
-
-        }
+        
 
         
     }
